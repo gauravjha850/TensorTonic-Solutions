5c84cbbfea73cb7c13556828758eabe220f5ffbd
gauravjha850 <gauravjha850@gmail.com>
2026-04-23 14:33:57 +0530

    Update solution for majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -4,9 +4,8 @@ def majority_classifier(y_train, X_test):
     classes,counts=np.unique(y_train,return_counts=True)
     majority_index=np.argmax(counts)
     majority_class=classes[majority_index]
-
-    predictions=np.full(len(X_test),majority_class,dtype =int)
-
+    predictions=np.full(len(X_test),majority_class,dtype=int)
     return predictions
     
+
     
